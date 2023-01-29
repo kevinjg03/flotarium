b774c9fa98bc0a479bc71a69033ad49e62e73802
kevinjg03 <mkjg16@gmail.com>
2023-01-28 19:18:19 -0600

    Initial Home page, variables css, not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogInternalComponent } from './blog-internal/blog-internal.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: 'blog', component: BlogComponent },
+  { path: 'article/:id', component: BlogInternalComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
